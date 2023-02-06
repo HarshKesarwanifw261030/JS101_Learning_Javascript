f0b28615dca38efede662cd872927be055f15743
HarshKesarwanifw261030 <harshkesarwani69.5@gmail.com>
2023-02-06 19:35:07 +0530

    soluntion of strings

---
+++ Javascript-IX Strings (IW)/problem1.7.js
@@ -0,0 +1,8 @@
+let name = "ram mohan shyam";
+let space=0;
+for(let i=0;i<name.length;i++){
+  if(name[i]==" "){
+    space++;
+  }
+}
+console.log("words:",space+1);
---
+++ Javascript-IX Strings (IW)/problem2.7.js
@@ -0,0 +1,6 @@
+let arr=["indian","army"];
+let characters=0;
+for(let i=0;i<arr.length;i++){
+  characters= characters+arr[i].length
+}
+console.log(characters);
